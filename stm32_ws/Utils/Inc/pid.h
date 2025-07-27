32bf3cee6527f840ed791e582829cfda948a16a0
AliiRezaei <alii.rezzaei@gmail.com>
2025-07-27 01:55:16 -0400

    Chng timestamp_prev type to TickType_t

--- stm32_ws/Utils/Inc/pid.h
+++ stm32_ws/Utils/Inc/pid.h
@@ -1,15 +1,50 @@
 /*
  * pid.h
  *
- *  Created on: Jul 27, 2025
- *      Author: ali
+ *  Created on: Apr 23, 2025
+ *      Author: Ali Rezaei
  */
 
-#ifndef UTILS_INC_PID_H_
-#define UTILS_INC_PID_H_
+#ifndef PIDCONTROLLER_INC_PID_H_
+#define PIDCONTROLLER_INC_PID_H_
+
+#define _constrain(amt,low,high) ((amt)<(low)?(low):((amt)>(high)?(high):(amt)))
+
+/*
+ * @brief PID Controller Structure
+*/
+typedef struct
+{
+	float P;         // P gain
+	float I;         // I gain
+	float D;         // D gain
+	float ramp;      // maximum speed of change of the output value
+	float sat;       // saturation (maximum allowable output value)
+	float e_prev;    // previous error value
+	float integral_prev;  // integral  of error
+	float output_prev;  // output
+	TickType_t timestamp_prev;
+} PIDController_s;
+
+/*
+ * @brief PID Controller Init
+*/
+void pid_Init(PIDController_s *pid);
+
+/*
+ * @brief PID Controller Set Gains
+*/
+void pid_SetGains(PIDController_s *pid, float p, float i, float d, float ramp, float sat);
+
+/*
+ * @brief PID Controller Operator
+*/
+float pid_Operator(PIDController_s *pid, float e);
+
+/*
+ * @brief PID Controller Reset
+*/
+void pid_Reset(PIDController_s *pid);
 
 
-
-
-
-#endif /* UTILS_INC_PID_H_ */
+#endif /* PIDCONTROLLER_INC_PID_H_ */

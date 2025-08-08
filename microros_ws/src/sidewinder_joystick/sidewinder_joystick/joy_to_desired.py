3c102f5e914ac6e897acfb6fe36a8e23e718ea23
AliiRezaei <alii.rezzaei@gmail.com>
2025-08-08 09:21:57 -0400

    Fix joint name issue

--- microros_ws/src/sidewinder_joystick/sidewinder_joystick/joy_to_desired.py
+++ microros_ws/src/sidewinder_joystick/sidewinder_joystick/joy_to_desired.py
@@ -7,10 +7,10 @@ class JoyToJoint(Node):
     def __init__(self):
         super().__init__('joy_to_joint')
         self.sub = self.create_subscription(Joy, 'joy', self.cb, 10)
-        self.pub = self.create_publisher(JointState, '/desired_states', 10)
-        self.joint_names = ['wrist_joint_0','wrist_joint_1','wrist_joint_2']
+        self.pub = self.create_publisher(JointState, '/joint_desireds', 10)
+        self.joint_names = ['joint1, joint2, joint3']
         # tune these scales for comfortable motion range
-        self.scales = [0.25, 0.25, 0.25]
+        self.scales = [0.25, 0.5, 1.0]
 
     def cb(self, msg: Joy):
         # map joystick axes 0,1,2 to joints (adjust indices as needed)
@@ -19,9 +19,19 @@ class JoyToJoint(Node):
         j.header.stamp = self.get_clock().now().to_msg()
         j.name = self.joint_names
         j.position = [
-            axes[0] * self.scales[2],
+            axes[2] * self.scales[2],
             axes[1] * self.scales[1],
-            axes[2] * self.scales[0]
+            axes[0] * self.scales[0]
+        ]
+        j.velocity = [
+            0.0,
+            0.0,
+            0.0
+        ]
+        j.effort = [
+            0.0,
+            0.0,
+            0.0
         ]
         self.pub.publish(j)
 

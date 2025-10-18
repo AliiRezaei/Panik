b76e6413d69cc1ea90d5e966de51c564f7aa37b5
AliiRezaei <alii.rezzaei@gmail.com>
2025-10-18 04:13:38 -0400

    Fix desired column

--- microros_ws/src/sidewinder_joystick/sidewinder_joystick/joy_to_desired.py
+++ microros_ws/src/sidewinder_joystick/sidewinder_joystick/joy_to_desired.py
@@ -7,7 +7,9 @@ class JoyToJoint(Node):
         super().__init__('joy_to_joint')
         self.sub = self.create_subscription(Joy, 'joy', self.cb, 10)
         self.pub = self.create_publisher(JointState, '/joint_desireds', 10)
-        self.joint_names = ['joint1, joint2, joint3']
+        self.joint_names = ['joint1',
+                            'joint2',
+                            'joint3']
         # tune these scales for comfortable motion range
         self.scales = [-0.25, 0.5, 1.0]
 
@@ -32,6 +34,20 @@ class JoyToJoint(Node):
             0.0,
             0.0
         ]
+
+        # j.position[0] = axes[2] * self.scales[2]
+        # j.position[1] = axes[1] * self.scales[1]
+        # j.position[2] = axes[0] * self.scales[0]
+
+        # j.velocity[0] = 0.0
+        # j.velocity[1] = 0.0
+        # j.velocity[2] = 0.0
+
+        # j.effort[0] = 0.0
+        # j.effort[1] = 0.0
+        # j.effort[2] = 0.0
+
+
         self.pub.publish(j)
 
 def main(args=None):

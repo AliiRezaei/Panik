ba38eda039a53bddfceb77d6c972d8c1d45d4fe3
AliiRezaei <alii.rezzaei@gmail.com>
2026-02-19 17:23:18 +0330

    Imp Joystick as a commander

--- matlab_ws/Imp/InitJoystick.m
+++ matlab_ws/Imp/InitJoystick.m
@@ -24,15 +24,6 @@ inputLayerSizeCtrl  = n + p;
 hiddenLayerSizeCtrl = 20;
 outputLayerSizeCtrl = p;
 
-%% Path Generation (Min Jerk)
-
-q1WayPoints = [0, 0.75,  0.25,  -0.7, 0];
-q2WayPoints = [0,  0.5,  -0.3,  0.45, 0];
-q3WayPoints = [0, -0.3,   0.1,  0.25, 0];
-
-wayPoints  = [q1WayPoints; q2WayPoints; q3WayPoints];
-timePoints = [0, 12, 27, 44, 60];
-
 %% Initial Conditions
 
 % observer network weights init conds
